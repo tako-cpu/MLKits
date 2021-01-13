01e5d5f277f8daa69e749e44c2b5ae2f66e3bb76
John Damian <raxoc52383@idcbill.com>
2021-01-13 10:11:44 +0800

    Added array for features

--- plinko/score.js
+++ plinko/score.js
@@ -1,5 +1,9 @@
+const outputs = [];
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+  
+  console.log(outputs);
 }
 
 function runAnalysis() {
