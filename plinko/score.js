da52ad26378039d782c8b60da79ef05527bdf672
John Damian <raxoc52383@idcbill.com>
2021-01-13 13:48:47 +0800

    Added split dataset

--- plinko/score.js
+++ plinko/score.js
@@ -1,12 +1,28 @@
 const outputs = [];
+const prediction = 300;
+const k = 3;
+const testData = [];
+const trainingData = [];
+const testcount = 0;
+
+
+function splitDataset()
+{
+	let shuffled = _.shuffle(outputs);
+	testData = _.slice(shuffled, 0, testcount);
+	trainingData = _.slice(shuffled, testcount);
+}
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
-  
-  console.log(outputs);
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+  const parsed = _.chain(outputs).map((output) => [Math.abs(output[0] - prediction), output[3]])
+				 .sortBy((output) => output[0]).slice(0, k)
+				 .countBy((output) => output[1]).toPairs().sortBy((pair) => pair[1])
+				 .last().first().parseInt().value();
+				 
+  console.log(parsed);
 }
 
